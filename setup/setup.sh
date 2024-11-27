318cc2f65452a72181b9305ad4fec8d6d094f43b
mariavillalba06 <mariadeangeles131@gmail.com>
2024-11-26 21:27:19 -0300

    cluster

--- setup/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ******************************************************
-echo Starting Replicaset
-echo ******************************************************
-# Sleep to wait for all mongo nodes to be ready
-sleep 10 | echo Sleeping 
-# Run script to initiate Replicaset
-mongo mongodb://node1:27017 rsconf.js
