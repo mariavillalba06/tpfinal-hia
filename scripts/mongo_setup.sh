65bdcd979bd65d0ddd28cc13704c759a74b96957
mariavillalba06 <mariadeangeles131@gmail.com>
2024-11-28 00:00:32 -0300

    configuracion git

--- scripts/mongo_setup.sh
+++ scripts/mongo_setup.sh
@@ -8,8 +8,9 @@ mongosh --host mongo-primary:27017 <<EOF
 rs.initiate({
     _id: "rs0",
     members: [
-        { _id: 0, host: "mongo-primary:27017" },
-        { _id: 1, host: "mongo-secondary:27017" }
+        { _id: 0, host: "mongo-primary:27017","priority": 2 },
+        { _id: 1, host: "mongo-secondary:27017","priority": 0 },
+        { _id: 2, host: "mongo-secondary2:27017","priority": 0 }
     ]
 });
 EOF
